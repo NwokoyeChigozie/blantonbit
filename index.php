2c0ed4db0ddf30c423893ed171481d4143c8e187
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-11-02 14:31:45 +0100

    tawk.to

--- index.php
+++ index.php
@@ -20,3 +20,16 @@ if(!isset($_SESSION['id'])){
 
 
 ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61813d5d6885f60a50b9f865/1fjgdfear';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
